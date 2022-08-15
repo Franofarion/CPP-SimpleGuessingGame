447ba8e3b8269df9d395e7ba38685c9835ee5a94
Baptiste Domange <baptiste.domange@gmail.com>
2022-08-15 20:49:58 +0200

    Initial Commit

---
+++ SimpleGuessingGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleGuessingGame
+//
+//  Created by Baptiste  Domange on 15/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

e35dadee5a2ac4620f6cd2d3e841e023ae29d3cf
Baptiste Domange <baptiste.domange@gmail.com>
2022-08-15 21:07:56 +0200

    rand a number & diplay it

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Guessing Game
+
+### Reminders 
+
+Convert a the `long` type provided by `time()` to an integer thanks to `static_cast<unsigned int>` syntax.
+
--- SimpleGuessingGame/main.cpp
+++ SimpleGuessingGame/main.cpp
@@ -5,10 +5,30 @@
 //  Created by Baptiste  Domange on 15/08/2022.
 //
 
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+// The entry point for our program when using GCC
+int main()
+{
+    // Generate unique random numbers using the current time
+    srand(static_cast<unsigned int>(time(NULL)));
+    // Get a random number between 0 and 99
+    unsigned int numberToGuess = rand() % 100;
+    
+    cout << "Guess a number between 0 and 99" << endl;
+    
+    unsigned int playersNumber {};
+    cin >> playersNumber;
+    
+    cout << "You guessed: "
+        << playersNumber
+        << " The actual number was: "
+        << numberToGuess
+        << endl;
     return 0;
 }
